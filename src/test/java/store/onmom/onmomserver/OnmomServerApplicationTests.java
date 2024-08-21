21ffdf48537625a7b4df6fea5087e9b612d4a9c7
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-08-21 22:45:48 +0900

    application 초기설정

---
+++ src/test/java/store/onmom/onmomserver/OnmomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package store.onmom.onmomserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnmomServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
