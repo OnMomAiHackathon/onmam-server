d9c982cfc28e8bd5203d897c6ade0be361850ae3
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-08-24 16:37:55 +0900

    기능:필터적용

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -2,9 +2,13 @@ package controller;
 
 import dto.auth.login.LoginRequest;
 import dto.auth.login.LoginResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import service.UserService;
 
 @RestController
@@ -15,8 +19,10 @@ public class AuthController {
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest request, HttpSession session) {
         LoginResponse response = userService.loginUser(request);
+        // 로그인 성공 시 세션에 사용자 정보 저장
+        session.setAttribute("user", response);
         return ResponseEntity.ok(response);
     }
 }
