6a3f460e0e887c4451626728644775ff05db759f
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-08-24 14:08:39 +0900

    기능:회원가입

--- src/main/java/repository/invitation/InvitationCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package repository.invitation;
-
-import entity.invitation.OnmomInvitationCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvitationCodeRepository extends JpaRepository<OnmomInvitationCode,Long> {
-}
