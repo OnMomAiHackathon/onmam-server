fbee36628a39d684b4fcdfbecf36d73aff4b99b8
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-08-24 17:10:35 +0900

    기능:카카오로그인 추가

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import dto.auth.login.KakaoLoginRequest;
 import dto.auth.login.LoginRequest;
 import dto.auth.login.LoginResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import service.KakaoAuthService;
 import service.UserService;
 
 @RestController
@@ -13,6 +15,7 @@ import service.UserService;
 @RequestMapping("auth")
 public class AuthController {
     private final UserService userService;
+    private final KakaoAuthService kakaoAuthService;
 
     // 로그인
     @PostMapping("/login")
@@ -28,5 +31,17 @@ public class AuthController {
     public ResponseEntity<Void> logout(HttpSession session) {
         session.invalidate();  // 세션 무효화
         return ResponseEntity.ok().build();
+    }
+
+
+    // 카카오톡 회원가입 및 로그인
+    @PostMapping("/kakao")
+    public ResponseEntity<LoginResponse> kakaoLogin(@RequestBody KakaoLoginRequest request, HttpSession session) {
+        // 카카오 OAuth2.0을 통해 사용자 정보 가져오기
+        LoginResponse response = kakaoAuthService.kakaoLogin(request.getAccessToken());
+
+        // 로그인 성공 시 세션에 사용자 정보 저장
+        session.setAttribute("user", response);
+        return ResponseEntity.ok(response);
     }
 }
