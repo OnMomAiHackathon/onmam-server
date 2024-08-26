7f97a3068ae0f769dde0f00b67ea65e75010fe68
JiAnJong <jja6312@naver.com>
2024-08-26 17:44:36 +0900

    삭제:쿠키 없앰

--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -18,24 +18,28 @@ public class LoginFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        HttpSession session = httpRequest.getSession(false);
 
+        // 모든 요청을 필터 없이 통과
+        chain.doFilter(request, response);
+
+        // 원래의 로그인 검증 코드가 있던 부분을 주석
+        /*
+        HttpSession session = httpRequest.getSession(false);
         String loginURI = httpRequest.getContextPath() + "/auth/login";
         boolean loggedIn = (session != null && session.getAttribute("user") != null);
         boolean loginRequest = httpRequest.getRequestURI().equals(loginURI);
 
         boolean isGetRequest = httpRequest.getMethod().equalsIgnoreCase("GET");
         if (loggedIn || loginRequest || !isGetRequest) {
-            // 이미 로그인되어 있거나 로그인 요청일 경우 또는 GET 요청이 아닌 경우 필터 체인 진행
             chain.doFilter(request, response);
         } else {
-            // 로그인되지 않은 사용자가 GET 요청을 보낼 경우 로그인 페이지로 리다이렉트
             httpResponse.sendRedirect(loginURI);
         }
-
+        */
     }
 
     @Override
