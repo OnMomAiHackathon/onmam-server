65537df7a71535843992390c955b5120be087304
JiAnJong <jja6312@naver.com>
2024-08-28 10:21:35 +0900

    기능:그림일기 월별조회

--- src/main/java/repository/diary/DiaryEntryRepository.java
+++ src/main/java/repository/diary/DiaryEntryRepository.java
@@ -4,8 +4,11 @@ import entity.diary.OnmomDiaryEntry;
 import entity.group.OnmomGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DiaryEntryRepository extends JpaRepository<OnmomDiaryEntry,Long> {
     List<OnmomDiaryEntry> findByGroup(OnmomGroup group);
+
+    List<OnmomDiaryEntry> findByGroupAndCreatedAtBetween(OnmomGroup group, LocalDate startDate, LocalDate endDate);
 }
